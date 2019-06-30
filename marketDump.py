51f55ba8647238595594d9b74e8eafbc558bc953
Arkahm <46363392+Arkahm@users.noreply.github.com>
2019-06-30 14:45:36 -0700

    made it work
    commented out loop and made sure list was created.

--- marketDump.py
+++ marketDump.py
@@ -4,7 +4,8 @@ import json
 url = 'https://esi.evetech.net/latest/markets/10000003/history/?datasource=tranquility&type_id=34'
 
 the_forge = requests.get(url)
-data = the_forge.json()
-
-for key, value in data.item(data):
-    print(key +':', value )
+data = the_forge.json()[0:1]
+print(data)
+#for key, value in data.item():
+#    print('date +':', value )
+    
