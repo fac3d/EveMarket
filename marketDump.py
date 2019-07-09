eb0877f8f3a885aac7bdc92ea1ff4c9e7159cb4d
Arkahm <46363392+Arkahm@users.noreply.github.com>
2019-07-08 17:45:17 -0700

    Working to find Sales to Volume
    Finally got it to sum the values by making 2 empty lists and appending them in the for loop.

--- marketDump.py
+++ marketDump.py
@@ -2,21 +2,32 @@ import requests
 import json
 import math
 
-Amarr='10000043'
+#Amarr='10000043'
 #Dodixie=
 #Hek=
 #Jita=
 #Rens=
 
-url = 'https://esi.evetech.net/latest/markets/10000043/history/?datasource=tranquility&type_id=35'
+url = 'https://esi.evetech.net/latest/markets/10000043/history/?datasource=tranquility&type_id=10631'
 
 region = requests.get(url)
 allMarkets = region.json()
-
+volumes=[]
+sales=[]
 for products in allMarkets[-7:]:
-    print(products['date'],' volume per day:', products['volume'])
+    #print(products['date'],' volume per day:', products['volume'])
+    volumes.append(products['volume'])
+    sales.append(products['order_count'])
+
+avg_7day_vol= sum(volumes)/7
+avg_7day_sales= sum(sales)/7
+SVR= (avg_7day_sales/avg_7day_vol)*100
 
 print('')
-print(type(products['volume']))
+print('Average 7-Day Volume=',avg_7day_vol)
+print('Average 7-Day Sales=',avg_7day_sales)
+print('Sales to Volume Ratio (%) =', SVR)
+print('')
+print('volume=',type(products['volume']))
 print('allMarkets=',type(allMarkets))
 print('products=',type(products))
