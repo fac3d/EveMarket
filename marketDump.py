28f7cd29149d2951c3c10a722bd330dbbf871298
Arkahm <46363392+Arkahm@users.noreply.github.com>
2019-07-10 20:28:35 -0700

    Update marketDump.py

--- marketDump.py
+++ marketDump.py
@@ -8,20 +8,23 @@ import math
 #Jita=
 #Rens=
 
-url = 'https://esi.evetech.net/latest/markets/10000043/history/?datasource=tranquility&type_id=10631'
+def item_srv():
 
-region = requests.get(url)
-allMarkets = region.json()
-volumes=[]
-sales=[]
-for products in allMarkets[-7:]:
-    #print(products['date'],' volume per day:', products['volume'])
-    volumes.append(products['volume'])
-    sales.append(products['order_count'])
+    url = 'https://esi.evetech.net/latest/markets/10000043/history/?datasource=tranquility&type_id=10631'
 
-avg_7day_vol= sum(volumes)/7
-avg_7day_sales= sum(sales)/7
-SVR= (avg_7day_sales/avg_7day_vol)*100
+    region = requests.get(url)
+    allMarkets = region.json()
+    volumes=[]
+    sales=[]
+    for products in allMarkets[-7:]:
+        #print(products['date'],' volume per day:', products['volume'])
+        volumes.append(products['volume'])
+        sales.append(products['order_count'])
+    
+    avg_7day_vol= sum(volumes)/7
+    avg_7day_sales= sum(sales)/7
+    SVR= (avg_7day_sales/avg_7day_vol)*100
+    return SRV
 
 print('')
 print('Average 7-Day Volume=',avg_7day_vol)
