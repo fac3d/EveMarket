5ec815da0a9d1968c2385be59fa652fecc0c3f29
Arkahm <46363392+Arkahm@users.noreply.github.com>
2019-07-11 12:07:50 -0700

    Update marketDump.py

--- marketDump.py
+++ marketDump.py
@@ -2,37 +2,45 @@ import requests
 import json
 import math
 
-#Amarr='10000043'
-#Dodixie=
-#Hek=
-#Jita=
-#Rens=
+Amarr='https://esi.evetech.net/latest/markets/10000043/history/?datasource=tranquility&type_id=3536'
+Dodixie='https://esi.evetech.net/latest/markets/10000032/history/?datasource=tranquility&type_id=17634'
+Hek='https://esi.evetech.net/latest/markets/10000042/history/?datasource=tranquility&type_id=17634'
+Jita='https://esi.evetech.net/latest/markets/10000002/history/?datasource=tranquility&type_id=17634'
+Rens='https://esi.evetech.net/latest/markets/10000030/history/?datasource=tranquility&type_id=17634'
 
-def item_srv():
-
-    url = 'https://esi.evetech.net/latest/markets/10000043/history/?datasource=tranquility&type_id=10631'
+def product_svr(station):
+    url = station
 
     region = requests.get(url)
     allMarkets = region.json()
     volumes=[]
     sales=[]
     for products in allMarkets[-7:]:
-        #print(products['date'],' volume per day:', products['volume'])
+        #print('sales per day:', products['order_count'])
+        print(products['date'],' volume per day:', products['volume'])
+        #print(products)
         volumes.append(products['volume'])
         sales.append(products['order_count'])
-    
-    avg_7day_vol= sum(volumes)/7
+
     avg_7day_sales= sum(sales)/7
-    SVR= (avg_7day_sales/avg_7day_vol)*100
-    return SRV
+    avg_7day_vol= sum(volumes)/7
+    ratio= (avg_7day_sales/avg_7day_vol)*100
+    #print('')
+    #print('Average 7-Day Sales=',avg_7day_sales)
+    #print('Average 7-Day Volume=',avg_7day_vol)
+    #print('')
+    #print('volume=',type(products['volume']))
+    #print('allMarkets=',type(allMarkets))
+    #print('products=',type(products))
+    return ratio
 
-item_srv()
-
-print('')
-print('Average 7-Day Volume=',avg_7day_vol)
-print('Average 7-Day Sales=',avg_7day_sales)
-print('Sales to Volume Ratio (%) =', SVR)
-print('')
-print('volume=',type(products['volume']))
-print('allMarkets=',type(allMarkets))
-print('products=',type(products))
+SVR_A = product_svr(Amarr)
+#SVR_J = product_svr(Jita)
+#SVR_R = product_svr(Rens)
+#SVR_D = product_svr(Dodixie)
+#SVR_H = product_svr(Hek)
+print('Amarr Sales to Volume Ratio (%) =', SVR_A)
+#print('Jita Sales to Volume Ratio (%) =', SVR_J)
+#print('Rens Sales to Volume Ratio (%) =', SVR_R)
+#print('Dodixie Sales to Volume Ratio (%) =', SVR_D)
+#print('Hek Sales to Volume Ratio (%) =', SVR_H)
