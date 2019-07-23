c4ca080850ced9ba1466c5f834ed0bda17a67f47
Arkahm <46363392+Arkahm@users.noreply.github.com>
2019-07-22 17:43:56 -0700

    Update marketDump.py

--- marketDump.py
+++ marketDump.py
@@ -2,31 +2,42 @@
 
 import requests
 import json
-#import pandas as pd
+import pandas as pd
 from datetime import timedelta, date, datetime
 
-Amarr = '10000043'
+system = input('What trade hub would you like to query? ')
 
-url = 'https://esi.evetech.net/latest/markets/10000043/types/?datasource=tranquility&page=1'
+if system == 'Amarr':
+    region = '10000043'
+elif system == 'Jita':
+    region = '10000002'
+elif system == 'Hek':
+    region = '10000042'
+elif system == 'Rens':
+    region = '10000030'
+elif system == 'Dodixie':
+    region = '10000032'
+else:
+    print('That is not a valid trade hub.')
+
+url = 'https://esi.evetech.net/latest/markets/'+ region +'/types/?datasource=tranquility&page=1'
 item_list = requests.get(url)
 json_list = item_list.json()
 
 #print(type(json_list))
 #print(json_list)
-
+timeDiff = str(date.today() - timedelta(days=7))
 def product_total_sold(number):
-    url = 'https://esi.evetech.net/latest/markets/' + Amarr + '/history/?datasource=tranquility&type_id='+ str(number)
+    url = 'https://esi.evetech.net/latest/markets/' + region + '/history/?datasource=tranquility&type_id='+ str(number)
     region = requests.get(url)
     all_region_Markets = region.json()
 
-    # number of items sold per day
-    sales=[]
-
+    sales = []
     # find items sold per day
     for products_sold in all_region_Markets:
         #print(products_sold['date'],' sales per day:', products_sold['volume'])
         #print(products_sold)
-        if products_sold['date'] > str(date.today() - timedelta(days=7)):
+        if products_sold['date'] > timeDiff:
             sales.append(products_sold['volume'])
 
     weekly_sales= sum(sales)
@@ -34,17 +45,16 @@ def product_total_sold(number):
     return weekly_sales
 
 def product_total_added(number):
-    url2 = 'https://esi.evetech.net/latest/markets/' + Amarr +'/orders/?datasource=tranquility&order_type=sell&page=1&type_id='+ str(number)
+    url2 = 'https://esi.evetech.net/latest/markets/' + region +'/orders/?datasource=tranquility&order_type=sell&page=1&type_id='+ str(number)
     daily_items = requests.get(url2)
     all_products = daily_items.json()
 
     # number of items on market per day
     volumes=[]
-
     # Find total items added to market in 7 days.
     for items_total in all_products:
         #print('Items added per day:', items_total['volume_total'])
-        if items_total['issued'] > str(datetime.today() - timedelta(days=7)):
+        if items_total['issued'] > timeDiff:
             volumes.append(items_total['volume_total'])
 
     weekly_vol= sum(volumes)
@@ -61,9 +71,11 @@ for type_id in json_list:
         continue
 
     # Output SRV value
-    if SVR >= 75.0:
+    if SVR >= 100:
         count += 1
-        print('Item Id: ' + str(type_id) ,'Amarr Sales to Volume Ratio (%) =', SVR)
+        print('Item Id: ' + str(type_id), str(system) + 'Sales to Volume Ratio (%) =', int(SVR))
 
-print(count)
+print('')
+print(count,' Items')
+print('')
 print('End Items')
