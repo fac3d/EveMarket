cc1fbe23d7e3dddc5e981613666bf52b71c270e8
Arkahm <46363392+Arkahm@users.noreply.github.com>
2019-08-07 13:50:54 -0700

    Update market_file_import.py

--- market_file_import.py
+++ market_file_import.py
@@ -16,6 +16,7 @@ def product_total_sold(number):
     url = 'https://esi.evetech.net/latest/markets/' + Amarr + '/history/?datasource=tranquility&type_id='+ str(number)
     region = requests.get(url)
     all_region_Markets = region.json()
+    #print(all_region_Markets[0])
 
     sales = []
     # find items sold per day
@@ -33,7 +34,7 @@ def product_total_added(number):
     url2 = 'https://esi.evetech.net/latest/markets/' + Amarr +'/orders/?datasource=tranquility&order_type=sell&page=1&type_id='+ str(number)
     daily_items = requests.get(url2)
     all_products = daily_items.json()
-
+    #print(all_products[0])
     # number of items on market per day
     volumes=[]
     # Find total items added to market in 7 days.
@@ -48,11 +49,14 @@ def product_total_added(number):
     return weekly_vol
 
 file = 'Market_Orders.csv'
-data = pd.read_csv(file)
-
+data = pd.read_csv(file, index_col=0)
+print(datetime.today())
+#print(data.keys())
+#print(type(data[['TypeID']]))
 count = 0
 timeDiff = date.today() - timedelta(days=7)
-for type_id in data['TypeID']:
+df = pd.DataFrame()
+for type_id, item_name in data.iterrows():
     #try excludes any sold_items/0 issues
     try:
         sold_items = product_total_sold(int(type_id))
@@ -61,11 +65,15 @@ for type_id in data['TypeID']:
     except:
         continue
 
-    # Output SRV value
+    # Output SVR value
     if SVR >= 100 and added_items > 10:
         count += 1
-        print('Item Id: ' + str(int(type_id)) + ' Amarr Sales to Volume Ratio (%) =', int(SVR))
+        #df2 = pd.DataFrame([type_id, item_name['Item'], int(SVR)])
+        #print(df2)
+        print(str(int(type_id)) + ': ' + item_name['Item'] + ' Amarr Sales to Volume Ratio (%) =', int(SVR))
 
+print(datetime.today())
+#print(df)
 print('')
 print(count,' Items')
 print('')
